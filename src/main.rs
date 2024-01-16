931cefe50c572e3366c8830424be225d46674edc
lucley64 <luc.leydert@gmail.com>
2024-01-16 12:45:20 +0100

    Instance

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ clippy::unnecessary_wraps
 
 use anyhow::{Result, anyhow};
 use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
+use vulkanalia::Version;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::{Window, WindowBuilder};
+
+const PORTABILITY_MACOS_VERSION: Version = Version::new(1, 3, 216);
 
 fn main() -> Result<()> {
     pretty_env_logger::init();
@@ -40,18 +46,59 @@ fn main() -> Result<()> {
             _ => {}
         }
     });
+}
 
-    #[derive(Clone, Debug)]
-    struct App{}
-    impl App {
-        unsafe fn create(window: &Window) -> Result<Self>{
-            Ok(Self {})
-        }
+#[derive(Clone, Debug)]
+struct App {
+    entry: Entry,
+    instance: Instance,
+}
 
-        unsafe fn render(&mut self, window: &Window) -> Result<()> {
-            Ok(())
-        }
-
-        unsafe fn destroy(&mut self) {}
+impl App {
+    unsafe fn create(window: &Window) -> Result<Self> {
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
+
+    unsafe fn render(&mut self, window: &Window) -> Result<()> {
+        Ok(())
+    }
+
+    unsafe fn destroy(&mut self) {
+        self.instance.destroy_instance(None)
+    }
+}
+
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance> {
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan rust tutorial\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let mut extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let flags = if
+    cfg!(target_os = "macos") &&
+        entry.version()? >= PORTABILITY_MACOS_VERSION {
+        info!("Enabling extensions for macos portability");
+        extensions.push(vk::KHR_GET_PHYSICAL_DEVICE_PROPERTIES2_EXTENSION.name.as_ptr());
+        extensions.push(vk::KHR_PORTABILITY_ENUMERATION_EXTENSION.name.as_ptr());
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::empty()
+    };
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions)
+        .flags(flags);
+
+    Ok(entry.create_instance(&info, None)?)
 }
